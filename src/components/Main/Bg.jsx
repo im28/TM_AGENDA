55924b56c379ea9b9767e4c706de905b768d4314
ibrahim mohammed <himaboy.im@gmail.com>
2019-11-03 22:41:56 +0800

    semi fix

--- src/components/Main/Bg.jsx
+++ src/components/Main/Bg.jsx
@@ -18,24 +18,35 @@ let rolePram = {
 }
 let name = ["Toastmaster of the evening","Surgent At Arms","Grammarian","Ah-Counter" ,"G.Evaluator","Topic Master","Timer"]
 let icon = ["mic_none","bookmark","menu_book"]
-
+let secondRow ={
+    marginTop:"25rem",
+    gridTemplateColumns: "auto auto auto",
+    justifyItems: "end",
+    gridColumnGap:"0",
+    paddingLeft: "7.8rem",
+    paddingRight: "7.8rem"
+}
 const Bg = (props) => (
 
     <div className={classnames( styles.bg)}>
         <p className={styles.tag} style={{  marginTop: "5.5rem"}}>Roles</p>
         <p className={styles.dateHeader}>MONDAY 15TH OCTOBER</p>
-        <div className={classnames(styles.gridContainerRole ,"container")}>
-
-            <RoleContainer/>
-            <RoleContainer/>
-            <RoleContainer/>
+        <div className={classnames(styles.gridContainerSpeaker ,"container")}
+        style={{marginTop:"10rem"}}>
             <RoleContainer/>
             <RoleContainer/>
             <RoleContainer/>
             <RoleContainer/>
         </div>
-        <p className={styles.tag} style={{  marginTop: "2.3rem"}}>Speakers & Evaluators</p>
-        <div className={classnames(styles.gridContainerSpeaker  ,"container")}>
+        <div className={classnames(styles.gridContainerSpeaker ,"container")} 
+        style={secondRow}>
+            <RoleContainer/>
+            <RoleContainer/>
+            <RoleContainer/>
+        </div>
+        <p className={styles.tag} style={{  marginTop: "40rem"}}>Speakers & Evaluators</p>
+        <div className={classnames(styles.gridContainerSpeaker  ,"container")}
+        style={{marginTop:"45rem"}}>
             <RoleContainer/>
             <RoleContainer/>
             <RoleContainer/>
